44b5961ce110ed6e66c83f6789111b277184b551
yunusakin <info.yunus.akin@gmail.com>
2025-11-04 10:06:00 +0300

    Add environment variable support and configure new services in Docker

--- order-service/src/main/java/org/novacore/orderservice/domain/enumeration/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package org.novacore.orderservice.domain.enumeration;
-
-public enum OrderStatus {
-    PENDING,     // Order just created, not validated yet
-    CONFIRMED,   // User + product validated, order is accepted
-    COMPLETED,   // Order fulfilled successfully
-    CANCELLED    // Cancelled (user action or validation failure)
-}
